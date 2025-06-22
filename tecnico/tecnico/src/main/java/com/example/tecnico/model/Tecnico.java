6511dd24a077799a9a56d4d2783d567f5c71ebba
Abraham1439 <ab.abanto@duocuc.cl>
2025-06-22 18:58:28 -0400

    Update Tecnico.java

--- tecnico/tecnico/src/main/java/com/example/tecnico/model/Tecnico.java
+++ tecnico/tecnico/src/main/java/com/example/tecnico/model/Tecnico.java
@@ -19,7 +19,7 @@ public class Tecnico {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long idTecnico;
 
     @Column(nullable = false)
     private String nombre;
